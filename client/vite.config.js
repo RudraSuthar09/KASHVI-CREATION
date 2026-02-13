6551d235ddafad2b550458915863ee0f22f0998c
SUTHAR RUDRA ALPESHKUMAR <rudrasuthar09@gmail.com>
2026-02-14 00:14:10 +0530

    Added changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,19 +2,19 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: ['.mjs', '.js', '.jsx', '.json', '.ts', '.tsx'] // Add this line
   },
   server: {
-    port: 5173, // Change if needed
+    port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // Updated backend port
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
