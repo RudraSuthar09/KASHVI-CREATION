84352855d9f25e2a3940deb3459df78088503f71
SUTHAR RUDRA ALPESHKUMAR <rudrasuthar09@gmail.com>
2026-02-13 15:00:48 +0530

    Add axios dependency for deployment

--- server/server.js
+++ server/server.js
@@ -45,7 +45,12 @@ mongoose
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://kashvi-creation.vercel.app", // Will update this later
+      "https://kashvi-creation-rudrasuthar09.vercel.app" // Alternate URL
+    ],
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
